ab6a383dca19dcd1cb516a1f16f2ae0f92ea812c
Abdelrahman Ramadan <ramadanabderahman@gmail.com>
2025-06-13 19:58:55 +0200

    update

--- build.sh
+++ build.sh
@@ -5,5 +5,13 @@ set -o errexit
 # Install Python 3.11
 python3.11 -m pip install --upgrade pip
 
+# Install system dependencies
+apt-get update && apt-get install -y \
+    build-essential \
+    ffmpeg \
+    libsndfile1 \
+    portaudio19-dev \
+    python3-pyaudio
+
 # Install dependencies
 python3.11 -m pip install -r requirements.txt
