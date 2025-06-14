a6656ce6d10c3ca5644c4b748cc98c50fcd73b1c
Abdelrahman Ramadan <ramadanabderahman@gmail.com>
2025-06-15 00:28:19 +0200

    update

--- app.py
+++ app.py
@@ -812,9 +812,9 @@ def shutdown():
 
 if __name__ == '__main__':
     startup_interaction_id = get_interaction_id()
-    app.run(port=5000)
-    # port = int(os.environ.get('PORT', 10000))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run(port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
