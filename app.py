02a0f7e1911252ac51ebb929dfd7ae7b925ab88a
Abdelrahman Ramadan <ramadanabderahman@gmail.com>
2025-07-26 13:13:33 +0200

    update

--- app.py
+++ app.py
@@ -797,10 +797,13 @@ def serve_static(filename):
 def serve_resource(filename):
     return send_from_directory('resources', filename)
 
+# if __name__ == '__main__':
+#     startup_interaction_id = get_interaction_id()
+#     app.run(port=5000)
+#     port = int(os.environ.get('PORT', 5000))
+
+
 if __name__ == '__main__':
-    startup_interaction_id = get_interaction_id()
-    app.run(port=5000)
     port = int(os.environ.get('PORT', 5000))
-
-
+    app.run(host='0.0.0.0', port=port, debug=False)
 
